e678f99b19fb175ecb2e926a2c5834d715039582
danok21 <yd_40954@163.com>
2025-06-09 08:21:16 +0800

    feat: optimized

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import { setupComponents } from './utils/setup-components'
 
 import 'virtual:uno.css'
 
+// codemirror 相关依赖：样式
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/xq-light.css'
+import 'codemirror/theme/darcula.css'
+import 'codemirror/mode/css/css'
+import 'codemirror/addon/dialog/dialog.css'
+
 /* 每个页面公共css */
 import '@/assets/index.css'
 import '@/assets/less/theme.less'
